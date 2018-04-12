0d41d2a155ed241cd66dc7c1bbb5985126b084af
Robin <rlockyer98@gmail.com>
2018-04-12 16:00:18 +0100

    Added RA BNF file

---
+++ bnf/RABNF.lua
@@ -0,0 +1,101 @@
+[query] => [projection]
+[query] => [selection]
+[query] => [rename]
+[query] => [join]
+[query] => [setOperation]
+[query] => [string]
+[query] =>  '(' + [query] + ')'
+
+
+[projection] => [pi] + [attributeList] + '('+ [query] + ')'
+
+[attributeList] => [string]
+[attributeList] => [string] + [comma] + [attributeList]
+
+
+[selection] => [sigma] + ' ' + [condition] + ' (' + [query] + ')'
+
+[sigma] => 'SIGMA'
+[sigma] => 'σ'
+[sigma] => 'SELECT'
+
+[condition] => '(' + [condition] + ')'
+[condition] => [value] + [operator] + [value]
+[condition] => [condition] + ' AND ' + [condition]
+[condition] => [condition] + ' OR ' + [condition]
+[condition] => 'NOT ' + [condition]
+
+
+[rename] => [attRename]
+[rename] => [relRename]
+
+[attRename] => [rho] + ' ' + [field] + '/' + [string] + ' (' + [query] + ')'
+[relRename] => [rho] + ' ' + [string] + ' (' + [query] + ')'
+
+[rho] => 'RHO'
+[rho] => 'ρ'
+[rho] => 'RENAME'
+
+[join] => [joinSymbol] + '(' + [query] + [comma] + [query] + ')'
+
+[joinSymbol] => ' JOIN ' + [condition]
+[joinSymbol] => ' ⋈ ' + [condition]
+
+
+[setOperation] => [union]
+[setOperation] => [intersection]
+[setOPeration] => [cartesian]
+[setOperation] => [difference]
+
+
+[union] => [unionSymbol] + '(' + [query] + [comma] + [query] + ')'
+
+[unionSymbol] => 'UNION'
+[unionSymbol] => '∪'
+[unionSymbol] => 'U'
+
+[intersection] => [intersectSymbol] + '(' + [query] + [comma] + [query] + ')'
+
+[intersectSymbol] => 'INTERSECT'
+[intersectSymbol] => '∩'
+[intersectSymbol] => 'N'
+
+[cartesian] => 'X(' + [query] + [comma] + [query] + ')'
+
+[difference] => '-(' + [query] + [comma] + [query] + ')'
+
+[operator] => '='
+[operator] => '<>'
+[operator] => '>'
+[operator] => '<'
+[operator] => '<='
+[operator] => '>='
+
+[value] => [field]
+[value] => [numeric]
+[value] => [quote] + [string] + [quote]
+[value] => [null]
+
+[null] => 'NULL'
+
+[int] => [0-9]
+[int] => [int] + [0-9]
+
+[numeric] => [int]
+[numeric] => [int] + '.' + [int]
+
+[quote] => '\''
+[quote] => '"'
+
+[comma] => ','
+[comma] => ', '
+
+[char] => [a-zA-Z]
+[char] => [int]
+
+[string] => [char]
+[string] => [char] + [string]
+
+[table] => [string]
+[field] => [string]
+[field] => [string] + '.' + [string]
