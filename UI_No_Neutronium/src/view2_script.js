fa1c9af15296e64c9798a9f54abe61bdf8e10435
GitPaulo <paulo.santos98@hotmail.com>
2018-04-21 18:33:17 +0100

    work on view2

---
+++ UI_No_Neutronium/src/view2_script.js
@@ -0,0 +1,30 @@
+/*************
+* On Clicks  *  
+*************/
+function backToView1OnClick(){
+	window.location = "index.html";
+}
+
+function helpOnClick(){
+
+}
+
+function applyHeuristicOnClick(){
+
+}
+
+function backOnClick(){
+
+}
+
+function fowardOnClick(){
+
+}
+
+function automateOnClick(){
+
+}
+
+function generateQueryOnClick(){
+
+}
