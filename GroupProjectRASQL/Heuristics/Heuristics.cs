56de2cb8e5d38a33729b7a2349ff0576d350f504
DuncanFinnamore <36283707+DuncanFinnamore@users.noreply.github.com>
2018-04-23 21:20:10 +0100

    Completed H1 - without steping system
    Heuristic 1s done-ish

--- GroupProjectRASQL/Heuristics/Heuristics.cs
+++ GroupProjectRASQL/Heuristics/Heuristics.cs
@@ -31,9 +31,15 @@ namespace GroupProjectRASQL.Heuristics
             var dfsStack = new Stack<TreeNode<Operation>>();
             var tempStack = new Stack<TreeNode<Operation>>();
 
-            dfsStack.Push(currentChild);
-            while(dfsStack.Count > 0)
+            dfsStack.Push(currentChild); // Push start of tree
+            while(dfsStack.Count > 0) // Depth first traverse the tree
             {
+                /*while ( stepType != 0) // This is where the Step type check happens,
+                {
+                        if button press > break 
+                        if >> stepType = 0
+                }
+                */
                 TreeNode<Operation> node = dfsStack.Pop();
                 if (visitedNodes.Contains(node))
                 {
@@ -41,35 +47,47 @@ namespace GroupProjectRASQL.Heuristics
                 }
                 visitedNodes.Add(node);
                 ////
-                if (node.Data.GetType().Name == "Selection")
+                if (node.Data.GetType().Name == "Selection") // if selection
                 {
                     String currentParameter = node.Data.parameter.TreeToString();
-                    if (currentParameter.Contains("AND"))
+                    if (currentParameter.Contains("AND")) // if more than one parameter
                     {
-                        //split goes here
+                        String[] splitParameter = currentParameter.Split(new string[] { "AND" }, StringSplitOptions.None); // Split String on any ANDs
+                        TreeNode<Operation> currentNode = node; // create a current node variable for the swap
+                        TreeNode<Operation> finalNode = node.Child(0); // save the child of the node to be split - to allow for the final child/ parent to be set
+                        node.Data.parameter = new TreeNode<String>(splitParameter[0]);
+
+                        for (int x=1; x< splitParameter.Length; x++ )
+                        {
+                            TreeNode<Operation> selection = new TreeNode<Operation>(new Selection(new TreeNode<String>(splitParameter[x]))); // New selection
+                            currentNode.RemoveChildren(); // Remove old children from current node
+                            currentNode.AddChild(selection); // Add the new node as a Child to current node
+                            currentNode = selection; // set the current node to the new node incase there are more than 2 selections
+                        }
+                        currentNode.AddChild(finalNode); // set the child of the last node that has been split
+
+                        
+
                     };
                 }
                 ////
                 foreach (TreeNode<Operation> child in node.Children)
                 {
-                    if (!visitedNodes.Contains(child))
+                    if (!visitedNodes.Contains(child)) // If unvisited 
                     {
-                        tempStack.Push(child);
+                        tempStack.Push(child); // Pushes unvisted children to a stack
                     }
                 }
-                while (tempStack.Count != 0)// A second stack is used
+                while (tempStack.Count != 0)// A second stack is used to reverse the input ( left to right traversal rather than right to left )
                 {
                     dfsStack.Push(tempStack.Pop());
                 }
 
-                Console.WriteLine(node);
+                Console.WriteLine(node); // Temp Output - to be replaced with a call to the neutronium outputter - possibly
 
 
             }
-
-
-
-            return rootTree;
+            return rootTree; // Return pointer to the tree that has been edited.
         }
 
         public static void Heuristic2()
@@ -88,7 +106,10 @@ namespace GroupProjectRASQL.Heuristics
         {
 
         }
+        public static void NeutroniumOutputter()
+        {
 
+        }
 
 
 
