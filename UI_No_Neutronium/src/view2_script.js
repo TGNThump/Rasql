73dda405daf09a919214369d9f73e0916d568cd9
GitPaulo <paulo.santos98@hotmail.com>
2018-04-25 17:33:49 +0100

    Changes to view 2 + tree display

--- UI_No_Neutronium/src/view2_script.js
+++ UI_No_Neutronium/src/view2_script.js
@@ -1,6 +1,62 @@
 /*************
 * On Clicks  *  
 *************/
+var tree; // init tree - check view2_tree_display.js
+$(document).ready(function(){
+	tree_config = {
+	    chart: {
+	        container: "#tree-simple",
+	        levelSeparation:    30,
+	        siblingSeparation:  20,
+	        subTeeSeparation:   50,
+	        rootOrientation: "NORTH",
+
+	        node: { 
+	            HTMLclass: "node-draw",
+	            drawLineThrough: false
+	        },
+	        connectors: {
+	            type: "step",
+	            style: {
+	                "stroke-width": 2,
+	                "stroke": "#ccc"
+	            },
+	            stackIndent: 20	
+	        }
+	    },
+	    
+	    nodeStructure: {
+	        text: { name: "Node", desc: "subscript" },
+	        children: [
+	            {
+	                text: { name: "Node", desc: "subscript" },
+	                children: [
+			            {
+			                text: { name: "Node", desc: "subscript" },
+			            },
+			            {
+	                		text: { name: "Node", desc: "subscript" }
+	            		}
+	            	],
+	            },
+	            {
+	                text: { name: "Node", desc: "subscript" },
+	                children: [
+			            {
+			                text: { name: "Node", desc: "subscript" },
+			            },
+			            {
+	                		text: { name: "Node", desc: "subscript" }
+	            		}
+	            	],
+	            }
+	        ],
+	    }
+	};
+
+	tree = new Treant(tree_config, function() { alert( 'Tree Loaded' ) }, $ );
+});  
+
 function backToView1OnClick(){
 	window.location = "index.html";
 }
@@ -14,7 +70,7 @@ function applyHeuristicOnClick(){
 }
 
 function backOnClick(){
-
+	
 }
 
 function fowardOnClick(){
