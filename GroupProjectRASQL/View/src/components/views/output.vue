c83c8064d76e39fec0e3e013b4ec359532b1edc9
Ben Pilgrim <ben@terragaming.co.uk>
2018-04-28 02:09:53 +0100

    VIews

--- GroupProjectRASQL/View/src/components/views/output.vue
+++ GroupProjectRASQL/View/src/components/views/output.vue
@@ -1,6 +1,111 @@
 <template>
 	<div>
+		<div class="row" style="margin-bottom: 0px;">
+			<column>
+				<div class="card d-flex flex-row" style="margin-bottom: 0px; border-bottom-radius: 0px;">
+					<div class="card-body" style="padding: 15px; text-align: right; width: 75px; background-color: #f7f7f7;">
+						SQL
+					</div>
+					<div class="col card-body" style="padding: 15px;">
+						SELECT * FROM test WHERE a=b AND b=c AND d=e;
+					</div>
+				</div>
+			</column>
+		</div>
+
+		<div class="row" style="margin-bottom: 0px;">
+			<column>
+				<div class="card d-flex flex-row" style="border-top-radius: 0px; border-top-width: 0px;">
+					<div class="card-body" style="padding: 15px; text-align: right; width: 75px; background-color: #f7f7f7;">
+						RA
+					</div>
+					<div class="col card-body" style="padding: 15px;">
+						RA: σ a=b AND b=c AND d=e (test)
+					</div>
+				</div>
+			</column>
+		</div>
+
 		<div class="row">
+			<column>
+				<div class="card">
+					<div class="card-body" style="height: 500px;">
+
+					</div>
+				</div>
+			</column>
+			<column>
+				<div class="row d-flex flex-row">
+					<column style="flex-grow: 0; padding-right: 0px;">
+						<button style="height: 100%; border-top-right-radius: 0px; border-bottom-right-radius: 0px; background-color: #F7F7F7;" class="btn btn-default">&lt;</button>
+					</column>
+					<column style="padding: 0px;">
+						<div class="card" style="margin-bottom: 0px; border-radius: 0px;">
+							<div class="card-header">The Selection Split Heuristic</div>
+							<div class="card-body">This deals with the splitting of any selection - σp(r) - statement that has more than one condition, for example  σa=b and b=c  ,into several smaller selections.</div>
+						</div>
+					</column>
+					<column style="flex-grow: 0; padding-left: 0px;">
+						<button style="height: 100%; border-top-left-radius: 0px; border-bottom-left-radius: 0px; background-color: #F7F7F7;" class="btn btn-default">&gt;</button>
+					</column>
+				</div>
+				<div class="row">
+					<column>
+						<button class="btn btn-block btn-primary">Step</button>
+					</column>
+					<column>
+						<button class="btn btn-block btn-primary">Complete</button>
+					</column>
+					<column>
+						<button class="btn btn-block btn-primary">Reset</button>
+					</column>
+				</div>
+				<div class="row">
+					<div class="input-group mb-1" style="margin-bottom: 0px;">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<input type="checkbox" aria-label="Checkbox for following text input">
+							</div>
+						</div>
+						<span class="form-control">Selection Split Heuristic</span>
+					</div>
+					<div class="input-group mb-1" style="margin-bottom: 0px;">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<input type="checkbox" aria-label="Checkbox for following text input">
+							</div>
+						</div>
+						<span class="form-control">Move Selection Heuristic</span>
+					</div>
+					<div class="input-group mb-1" style="margin-bottom: 0px;">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<input type="checkbox" aria-label="Checkbox for following text input">
+							</div>
+						</div>
+						<span class="form-control">Restriction Heuristic</span>
+					</div>
+					<div class="input-group mb-1" style="margin-bottom: 0px;">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<input type="checkbox" aria-label="Checkbox for following text input">
+							</div>
+						</div>
+						<span class="form-control">Equijoin Conversion Heuristic</span>
+					</div>
+					<div class="input-group mb-1" style="margin-bottom: 0px;">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<input type="checkbox" aria-label="Checkbox for following text input">
+							</div>
+						</div>
+						<span class="form-control">Move Projection Heuristic</span>
+					</div>
+				</div>
+			</column>
+		</div>
+
+		<!-- <div class="row">
 			<column>
 				<div v-html="model.output"></div>
 			</column>
@@ -12,7 +117,7 @@
 			<column>
 				<button class="btn btn-block btn-primary" @click="model.stepToEnd.Execute('')">END</button>
 			</column>
-		</div>
+		</div> -->
 	</div>
 </template>
 
--- GroupProjectRASQL/View/src/components/views/schema.vue
+++ GroupProjectRASQL/View/src/components/views/schema.vue
@@ -1,27 +1,104 @@
 <template>
 	<div class="container-fluid app d-flex flex-column">
-		Schema
+		<div class="row d-flex flex-row" style="flex-shrink: 0;">
+			<column>
+				<input class="form-control" type="text" placeholder="Relation"></input>
+			</column>
+			<column style="flex-grow: 0;">
+				<button type="button" class="btn btn-primary">New Relation</button>
+			</column>
+		</div>
+		<div class="row">
+			<column>
+				<div class='card' style='flex-shrink: 0;' v-for='relation in Relations'>
+					<div class='card-header' style="padding: 5px;">	
+						<div class="row d-flex flex-row" style="flex-shrink: 0; margin: 0px;">
+							<column>
+								<input class="form-control relationName" type="text" placeholder="Relation" v-model="relation.name"></input>
+							</column>
+							<column style="flex-grow: 0;">
+								<button type="button" style="margin: 0px;" class="btn btn-default">Delete Relation</button>
+							</column>
+						</div>
+					</div>
+					<div class='card-body'>
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th scope="col" v-for='field in relation.fields'>
+										<input class="form-control fieldName" type="text" placeholder="Field" v-model="field.name"></input>
+									</th>
+									<th scope="col" class="newButton">
+										<button @click="newColumn(relation)" type="button" class="btn btn-default">+</button>
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr v-for='(_,i) in relation.fields[0].values'>
+									<td v-for='field in relation.fields'>
+										<input class="form-control fieldValue" type="text" placeholder="Value" v-model="field.values[i]"></input>
+									</td>
+									<td class="newButton">
+										<button @click="removeRow(relation, i)" type="button" class="btn btn-default">-</button>
+									</td>
+								</tr>
+								<tr>
+									<td v-for='(field,i) in relation.fields'>
+										<button @click="removeColumn(relation, i)" :disabled="relation.fields.length < 2"  type="button" class="btn btn-block btn-default">-</button>
+									</td>
+									<td></td>
+								</tr>
+								<tr>
+									<td style="background-color: transparent !important; "class="newButton" :colspan="relation.fields.length">
+										<button @click="newRow(relation)" type="button" class="btn btn-block btn-default">+</button>
+									</td>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</column>
+		</div>
 	</div>
 </template>
 
 <script>
-	const props={
-		viewModel: Object,
-		__window__: Object
-	};
+const props={
+	viewModel: Object,
+	__window__: Object
+};
 
-	export default {
-		name: 'app',
-		props,
-		data () {
-			return this.viewModel
-		},
-		methods: {
+export default {
+	name: 'app',
+	props,
+	data () {
+		return this.viewModel
+	},
+	methods: {
 
-		}
 	}
+}
 </script>
 
 <style>
+.cardcontainer .card{
+	margin-top: 10px;
+}
 
+.fieldName,
+.relationName{
+	font-weight: bold;
+}
+
+.relationName,
+.fieldName,
+.fieldValue{
+	background-color: transparent; border: 0px;
+}
+
+.newButton{
+	text-align: center;
+	vertical-align: center;
+}
 </style>
